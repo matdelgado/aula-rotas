1c4c14f845ea9e60aa849d6d0686e0eb8756f9cc
matdelgado <matdelgado0@gmail.com>
2023-09-03 16:19:33 -0300

    Atualizando main.jsx para redirecionamento de endereço inutilizado.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
 import Home from './routes/Home/Home.jsx'
 import Produtos from './routes/Produtos/Produtos.jsx'
 import Error from './routes/Error/Error.jsx'
@@ -25,6 +25,10 @@ const router = createBrowserRouter([{
     {
       path:"produtos/editar/:id",
       element: <EditarProduto/>
+    },
+    {
+      path: "/antiga",
+      element: <Navigate to="/"/>
     }
   ]
 }])
