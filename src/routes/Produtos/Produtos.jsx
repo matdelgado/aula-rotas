8374538c7dd382c2054262af3fd83599a3539d48
matdelgado <matdelgado0@gmail.com>
2023-09-03 16:13:10 -0300

    Atualizando arquivo Produto.jsx

--- src/routes/Produtos/Produtos.jsx
+++ src/routes/Produtos/Produtos.jsx
@@ -1,7 +1,16 @@
 export default function Produtos() {
   return (
+  
     <main>
-        <h1>Produtos</h1>
+      <h1>Produtos</h1>
+      {ListaProdutos.map(prod => (
+        <div key={prod.id}>
+          <Link to={`/produtos/editar/${prod.id}`}>
+            Editar o produto: {prod.nome}
+          </Link>
+        </div>
+      ))}
     </main>
-  )
+  
+    )
 }
